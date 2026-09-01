5f5189473d6d37db544833be4652bcba3e3c4e4f
Shane <ShaneLin@uanalyze.com.tw>
2026-09-01 16:30:30 +0800

    Updates

--- assets/PracticeCylinder-937d49ad.js
+++ assets/PracticeCylinder-937d49ad.js
@@ -1,4 +1,4 @@
-import{r as Je,g as M1,j as ht}from"./index-47b0e121.js";/**
+import{r as Je,g as M1,j as ht}from"./index-82f8383c.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
